55555c85d604ce74d0747a0912b4a2bb9c7e6f61
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-02-07 17:01:15 +0000

    backing up the database

--- yarnStashDb.sql
+++ yarnStashDb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Generation Time: Feb 07, 2023 at 10:46 AM
+-- Generation Time: Feb 07, 2023 at 05:00 PM
 -- Server version: 5.7.41
 -- PHP Version: 8.0.19
 
@@ -45,15 +45,15 @@ CREATE TABLE `yarn` (
 --
 
 INSERT INTO `yarn` (`id`, `name`, `shade`, `dominantColour`, `composition`, `yarnType`, `lengthInMeters`, `skeinNumber`, `project`, `image`) VALUES
-(1, 'Sirdar Jewelspun', 'Midnight fjords', 'pink', 'Acrylic', 'Aran', 500, 2, 'Shawl ', NULL),
-(2, 'Sirdar Jewelspun', 'Golden Green', 'green', 'Acrylic', 'Aran', 500, 0.1, NULL, NULL),
-(3, 'Deramores Studio', 'Fuschia ', 'pink', 'Acrylic', 'Chunky', 80, 3, 'Diagonal weave blanket ', NULL),
-(4, 'Deramores Studio', 'Aubergine', 'purple', 'Acrylic', 'Chunky', 80, 1, 'Diagonal weave blanket', NULL),
+(1, 'Sirdar Jewelspun', 'Midnight Fjords', 'pink', 'Acrylic', 'Aran', 500, 0.66, NULL, 'pink_shawl.jpg'),
+(2, 'Sirdar Jewelspun', 'Golden Green', 'green', 'Acrylic', 'Aran', 500, 0.1, NULL, 'green_shawl.jpg'),
+(3, 'Deramores Studio', 'Fuschia ', 'pink', 'Acrylic', 'Chunky', 80, 6, 'Diagonal weave blanket ', NULL),
+(4, 'Deramores Studio', 'Aubergine', 'purple', 'Acrylic', 'Chunky', 80, 2, 'Diagonal weave blanket', NULL),
 (5, 'Scheepjes Chunky Monkey', 'Deep Turquoise', 'blue', 'Acrylic', 'Chunky', 116, 2, 'Slipper Socks', NULL),
 (6, 'King Cole Luxury Merino', 'Mustard', 'yellow', 'Merino wool', 'DK', 140, 20, 'Emerald valley jumper', NULL),
-(7, 'James C. Brett Party Time', 'PT2', 'blue', 'Acrylic', 'Chunky', 154, 1, 'Ear warmer', NULL),
-(8, 'Cotton cord 5mm', 'Metallic green', 'green', 'Cotton', 'Macrame cord 5mm', 100, 2, 'Basket', NULL),
-(9, 'Paintbox Yarns Wool Mix Super Chunky', 'Royal blue', 'blue', 'Wool and Acrylic', 'Super Chunky', 55, 1.5, NULL, NULL);
+(7, 'James C. Brett Party Time', 'PT2', 'blue', 'Acrylic', 'Chunky', 154, 0.5, 'Ear warmer', 'ear_warmer_in_progress.jpg'),
+(8, 'Cotton cord 5mm', 'Metallic green', 'green', 'Cotton', 'Macrame cord 5mm', 100, 0, 'Basket', 'green_basket.jpg'),
+(9, 'Paintbox Yarns Wool Mix Super Chunky', 'Royal blue', 'blue', 'Wool and Acrylic', 'Super Chunky', 55, 1.5, NULL, 'blue_scarf.jpg');
 
 --
 -- Indexes for dumped tables
