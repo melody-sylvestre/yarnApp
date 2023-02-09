893060cc22ea957192c7f7e95cf33b9ca8d9dc37
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-02-09 17:07:29 +0000

    all done - might need to check for random stuff though

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ $yarnItems = getItemsFromDb($db);
 <head>
     <title>Melody's yarn stash</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" type="text/css" rel="stylesheet">
-    <link href="styles/style.css" type="text/css" rel="stylesheet">           
+    <link href="styles/style.css" type="text/css" rel="stylesheet">
+    <!-- <link href="styles/advancedStyling.css" type="text/css" rel="stylesheet">            -->
 </head>
 
 <body>
